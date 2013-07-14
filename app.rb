dc649da649b68ca81443fa9ea2d8eec9a3a0a7dd
Steve Hoeksema <steve@seven.net.nz>
2013-07-15 09:39:17 +1200

    Updates

--- app.rb
+++ app.rb
@@ -1,16 +1,21 @@
 require "logger"
 require "sinatra"
+require "active_support/core_ext/object/to_query"
+
+configure do
+  set :bind, "0.0.0.0"
+end
 
 params_logger = Logger.new("params.log")
 
 get "/" do
-  params_logger.info params
+  params_logger.info params.to_query
 
   "Get OK"
 end
 
 post "/" do
-  params_logger.info params
+  params_logger.info params.to_query
 
   "Post OK"
 end
